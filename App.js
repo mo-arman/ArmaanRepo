5a21f266569794b527bd0f44de6420e570f87704
mo-arman <moharman3818@gmail.com>
2024-03-06 14:22:43 +0530

    Some changes

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-// const heading = React.createElement("h1",{id:"heading",
-// xyz:"abc"},"Hello world From React!");
-// console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-const parent  = React.createElement("div",{id:"parent"},
-React.createElement("div",{id:"child"},
-React.createElement("h1",{},"I am an h1 tag"),
-)
-);
+const parent = React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am an h1 tag"),
+        React.createElement("h2",{},"I am an h2 tag"),
+    ]),
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am an h1 tag"),
+        React.createElement("h2",{},"I am an h2 tag"),
+    ]),
+]);
+
 console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("header"));
 root.render(parent);
